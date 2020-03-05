8c85a3dbceaaba87a8da543ac9c189145bf10933
lazhcsilva <53198127+lazhcsilva@users.noreply.github.com>
2020-03-05 18:28:21 -0300

    Initial commit

---
+++ src/main/java/project/instantwork/InstantWorkApplication.java
@@ -0,0 +1,13 @@
+package project.instantwork;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InstantWorkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(InstantWorkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/project/instantwork/InstantWorkApplicationTests.java
@@ -0,0 +1,13 @@
+package project.instantwork;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class InstantWorkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
